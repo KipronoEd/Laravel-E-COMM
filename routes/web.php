50c979d272e41097f95ed1c94324d1814200ead2
Edwin Kiprono <edykiprono@gmail.com>
2020-11-29 22:56:54 +0300

    login done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
 Route::view('login', 'login');
+
+Route::post('/login', [UserController::class, 'login']);
+Route::get('/', [ProductController::class, 'index']);
